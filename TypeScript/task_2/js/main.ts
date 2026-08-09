2af1fbee8d72907d39a022d306907a8244fe8f49
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-08-09 20:26:36 +0000

    task_2: Creating functions specific to employees

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,52 +1,17 @@
-// Director üçün interfeys
-interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+// İşçinin Director olub-olmadığını yoxlayan Type Predicate funksiyası
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
 }
 
-// Teacher üçün interfeys
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-// Director sinfi
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return 'Working from home';
+// İşçinin tipinə uyğun tapşırığı icra edən funksiya
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
   }
-  getCoffeeBreak(): string {
-    return 'Getting a coffee break';
-  }
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-}
-
-// Teacher sinfi
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-}
-
-// İşçi yaradan funksiya
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
 }
 
 // Nəticəni yoxlamaq üçün (Şərtdəki Example)
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
